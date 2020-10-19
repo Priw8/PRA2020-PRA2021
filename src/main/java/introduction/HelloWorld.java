5b87ad944c69b96f20727fc0153b129dcf856727
Priw8 <priwe8@gmail.com>
2020-10-19 18:28:52 +0200

    test commit

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    static Logger logger = Logger.getLogger("name");
+
     public static void main(String [ ] args) {
+        logger.info("Hello from logger");
         System.out.println("Hello World");
     }
 }
